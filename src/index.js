682359faba499393e47dfb150880379e0d5ea41e
yabeizeng1121 <yz856@duke.edu>
2024-09-03 00:45:03 -0400

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Introduction from './Introduction';
 import './index.css';
